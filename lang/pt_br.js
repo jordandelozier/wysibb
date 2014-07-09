1990a852c676e72708666f9c07318f883170af99
Daniel Lemes <dlemes@gmail.com>
2014-07-09 16:56:10 -0300

    Add new language portuguese brazilian

---
+++ lang/pt_br.js
@@ -0,0 +1,72 @@
+/*Portuguese brazilian translation by lemesdaniel */
+if (typeof (WBBLANG)=="undefined") {WBBLANG = {};}
+WBBLANG['pt_br'] = {
+	bold: "Negrito",
+	italic: "Itálico",
+	underline: "Sublinhado",
+	strike: "Tachado",
+	link: "Link",
+	img: "Inserir uma imagem",
+	sup: "Sobrecrito",
+	sub: "Subscrito",
+	justifyleft: "Alinhado a esquerda",
+	justifycenter: "centralizado",
+	justifyright: "Alinhado a direita",
+	table: "Inserir uma tabela",
+	bullist: "• Lista não ordenada",
+	numlist: "1. Lista ordenada",
+	quote: "Citar",
+	offtop: "Offtop",
+	code: "Code",
+	spoiler: "Spoiler",
+	fontcolor: "Cor da fonte",
+	fontsize: "Tamanho da fonte",
+	fontfamily: "Tipo de fonte",
+	fs_verysmall: "Muito pequeno",
+	fs_small: "Pequeno",
+	fs_normal: "Normal",
+	fs_big: "Grande",
+	fs_verybig: "Muito grande",
+	smilebox: "Emoticons",
+	
+	modal_link_title: "Inserir um link",
+	modal_link_text: "Adicione um texto",
+	modal_link_url: "URL",
+	modal_email_text: "Adicione um email",
+	modal_email_url: "Email",
+	modal_link_tab1: "Inserir a URL",
+	
+	modal_img_title: "Inserir uma imagem",
+	modal_img_tab1: "Inserir URL",
+	modal_img_tab2: "Download da imagem",
+	modal_imgsrc_text: "Digite a URL da imagem",
+	modal_img_btn: "Escolha um arquivo",
+	add_attach: "Anexar um arquivo",
+	
+	close: "Fechar",
+	save: "Salvar",
+	cancel: "Cancelar",
+	remove: "Remover",
+	
+	validation_err: "Os dados digitados são inválidos",
+	error_onupload: "Erro ao carregar o arquivo",
+	
+	fileupload_text1: "Solte o arquivo aqui",
+	fileupload_text2: "ou",
+	
+	loading: "Carregando",
+	auto: "Auto",
+	views: "Visualizações",
+	downloads: "de downloads",
+	
+	//smiles
+	sm1: "Sorriso",
+	sm2: "Risada",
+	sm3: "wink",
+	sm4: "Obrigado",
+	sm5: "Repreender",
+	sm6: "Choque",
+	sm7: "Zangado",
+	sm8: "Dor",
+	sm9: "Doente"
+};
