c23512a2d221f0432453b0d23103f60301a9a0fe
ATofighi <alirtofighim@gmail.com>
2014-10-14 16:36:15 +0330

    Add Persian Language

---
+++ lang/fa.js
@@ -0,0 +1,79 @@
+﻿/*
+ Language: Persian - فارسی
+ By: AliReza_Tofighi - http://www.white-crow.ir
+*/
+if (typeof (WBBLANG)=="undefined") {WBBLANG = {};}
+WBBLANG['fa'] = CURLANG = {
+	bold: "ضخیم",
+	italic: "کج",
+	underline: "زیرخط‌دار",
+	strike: "خط‌خورده",
+	link: "پیوند",
+	img: "افزودن تصویر",
+	sup: "بالانویس",
+	sub: "زیرنویس",
+	justifyleft: "تراز به چپ",
+	justifycenter: "تراز به وسط",
+	justifyright: "تراز به راست",
+	table: "افزودن جدول",
+	bullist: "• لیست نقطه‌ای",
+	numlist: "1. لیست شماره‌ای",
+	quote: "نقل‌قول",
+	offtop: "متن خاموش",
+	code: "کد",
+	spoiler: "جعبه‌ی ضربه‌ای",
+	fontcolor: "رنگ قلم",
+	fontsize: "اندازه‌ی قلم",
+	fontfamily: "نوع قلم",
+	fs_verysmall: "خیلی کوچک",
+	fs_small: "کوچک",
+	fs_normal: "معمولی",
+	fs_big: "بزرگ",
+	fs_verybig: "خیلی بزرگ",
+	smilebox: "افزودن شکلک",
+	video: "افزودن ویدیو یوتوب",
+	removeFormat:"حذف فرمت",
+	
+	modal_link_title: "افزودن پیوند",
+	modal_link_text: "متن نمایشی",
+	modal_link_url: "آدرس",
+	modal_email_text: "رایانامه نمایشی",
+	modal_email_url: "رایانامه",
+	modal_link_tab1: "افزودن آدرسی",
+	
+	modal_img_title: "افزودن تصویر",
+	modal_img_tab1: "افزودن آدرسی",
+	modal_img_tab2: "آپلود تصویر",
+	modal_imgsrc_text: "آدرسی تصویر را وارد کنید",
+	modal_img_btn: "انتخاب فایل",
+	add_attach: "افزودن پیوست",
+	
+	modal_video_text: "آدرس ویدیو را وارد فرمائید",
+	
+	close: "بستن",
+	save: "ذخیره",
+	cancel: "لغو",
+	remove: "حذف",
+	
+	validation_err: "محتوای وارد شده نامعتبر است",
+	error_onupload: "خطایی در بارگذاری فایل به وجود آمده است",
+	
+	fileupload_text1: "فایل‌را به اینجا بکشید",
+	fileupload_text2: "یا",
+	
+	loading: "درحال بارگیری",
+	auto: "خودکار",
+	views: "بازدید‌ها",
+	downloads: "دریافت‌ها",
+	
+	//smiles
+	sm1: "لبخند",
+	sm2: "خنده",
+	sm3: "چشمک",
+	sm4: "سپاس",
+	sm5: "سرزنش",
+	sm6: "شوک",
+	sm7: "عصبانی",
+	sm8: "درد",
+	sm9: "بیمار"
+};
