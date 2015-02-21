ce87a7d3e2781608a6c3afbce00ee4f50a02e1e9
Mohammad Anbarestany <m_anbarestany@hotmail.com>
2015-02-21 20:55:34 +0330

    Update and rename lang to lang/fa.js
    Persian language for WysiBB

---
+++ lang/fa.js
@@ -0,0 +1,76 @@
+/*Farsi language by Tam2145@gmail.com :: tam2145@yahoo.com website: http://forums.socialtools.ir/ */
+if (typeof (WBBLANG)=="undefined") {WBBLANG = {};}
+WBBLANG['fa'] = {
+	bold: "درشت",
+	italic: "خمیده",
+	underline: "زیر خط دار",
+	strike: "خط خورده",
+	link: "پیوند",
+	img: "تصویر",
+	sup: "بالانویس",
+	sub: "زیرنویس",
+	justifyleft: "چپ چین",
+	justifycenter: "وسط چین",
+	justifyright: "راست چین",
+	table: "درج جدول",
+	bullist: "• لیست بدون ترتیب",
+	numlist: "1. لیست با ترتیب",
+	quote: "نقل قول",
+	offtop: "Offtop",
+	code: "کد",
+	spoiler: "پنهان کردن",
+	fontcolor: "رنگ متن",
+	fontsize: "اندازه قلم",
+	fontfamily: "نام قلم",
+	fs_verysmall: "خیلی کوچک",
+	fs_small: "کوچک",
+	fs_normal: "استاندارد",
+	fs_big: "بزرگ",
+	fs_verybig: "خیلی بزرگ",
+	smilebox: "درج خندانک",
+	video: "درج یوتیوب",
+	removeFormat:"حذف فرمت",
+	
+	modal_link_title: "درج پیوند",
+	modal_link_text: "متن نمایشی",
+	modal_link_url: "آدرس پیوند",
+	modal_email_text: "نمایش پست الکترونیکی",
+	modal_email_url: "پست الکترونیکی",
+	modal_link_tab1: "درج پیوند",
+	
+	modal_img_title: "درج تصویر",
+	modal_img_tab1: "درج پیوند",
+	modal_img_tab2: "پیوست تصویر",
+	modal_imgsrc_text: "درج پیوند تصویر",
+	modal_img_btn: "انتخاب فایل",
+	add_attach: "اضافه کردن پیوست",
+	
+	modal_video_text: "ورود پیوند برای ویدیو",
+	
+	close: "بستن",
+	save: "ذخیره",
+	cancel: "انصراف",
+	remove: "حذف",
+	
+	validation_err: "اطلاعات وارد شده نامعتبر است",
+	error_onupload: "خطا در آپلود فایل",
+	
+	fileupload_text1: "فایل را اینجا رها کنید",
+	fileupload_text2: "یا",
+	
+	loading: "در حال بارگیری",
+	auto: "خودکار",
+	views: "نمایش ها",
+	downloads: "دانلود",
+	
+	//smiles
+	sm1: "لبخند",
+	sm2: "خنده",
+	sm3: "چشمک",
+	sm4: "تشکر",
+	sm5: "سرزنش",
+	sm6: "شوک",
+	sm7: "عصبانی",
+	sm8: "درد",
+	sm9: "بیمار"
+};
