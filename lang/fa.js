309287da823d6564c19bef4e6c1db1f3eb9b1e71
WysiBB <vad85@mail.ru>
2015-05-05 13:26:58 +0300

    Farse language

---
+++ lang/fa.js
@@ -0,0 +1,77 @@
+//Thanks Exlord for transplate - https://github.com/wbb/wysibb/issues/74
+
+if (typeof (WBBLANG)=="undefined") {WBBLANG = {};}
+WBBLANG['fa'] = {
+    bold: "درشت",
+    italic: "خمیده",
+    underline: "زیرخطدار",
+    strike: "میانخط",
+    link: "پیوند",
+    img: "قرار دادن تصویر",
+    sup: "بالانویس",
+    sub: "زیرنویس",
+    justifyleft: "تراز چپ",
+    justifycenter: "تراز به مرکز",
+    justifyright: "تراز راست",
+    table: "قرار دادن جدول",
+    bullist: "• لیست نامرتب",
+    numlist: "1. لیست مرتب",
+    quote: "نقل قول",
+    offtop: "Offtop",
+    code: "کد",
+    spoiler: "Spoiler",
+    fontcolor: "رنگ فونت",
+    fontsize: "اندازه فونت",
+    fontfamily: "خانواده فونت",
+    fs_verysmall: "خیلی کوچک",
+    fs_small: "کوچک",
+    fs_normal: "معمولی",
+    fs_big: "بزرگ",
+    fs_verybig: "خیلی بزرگ",
+    smilebox: "قرار دادن شکلک",
+    video: "قرار دادن یوتیوب",
+    removeFormat:"حذف قالب",
+
+    modal_link_title: "قرار دادن لینک",
+    modal_link_text: "نمایش متن",
+    modal_link_url: "URL",
+    modal_email_text: "نمایش پست الکترونیک",
+    modal_email_url: "پست الکترونیک",
+    modal_link_tab1: "قرار دادن URL",
+
+    modal_img_title: "درج تصویر",
+    modal_img_tab1: "قرار دادن URL",
+    modal_img_tab2: "بارگذاری تصویر",
+    modal_imgsrc_text: "URL تصویر را وارد کنید",
+    modal_img_btn: "فایل را انتخاب کنید",
+    add_attach: "اضافه کردن فایل پیوست",
+
+    modal_video_text: "URL ویدئو را وارد کنید",
+
+    close: "بستن",
+    save: "ذخیره",
+    cancel: "لغو کردن",
+    remove: "حذف کردن",
+
+    validation_err: "اطلاعات وارد شده نامعتبر است",
+    error_onupload: "خطا در هنگام آپلود فایل",
+
+    fileupload_text1: "فایل را اینجا رها کنید",
+    fileupload_text2: "یا",
+
+    loading: "در حال بارگذاری",
+    auto: "اتوماتیک",
+    views: "نمایه ها",
+    downloads: "دانلود ها",
+
+    //smiles
+    sm1: "لبخند",
+    sm2: "خنده",
+    sm3: "چشمک",
+    sm4: "متشکرم",
+    sm5: "سرزنش",
+    sm6: "Shock",
+    sm7: "خشمگین",
+    sm8: "درد",
+    sm9: "بیمار"
+};
